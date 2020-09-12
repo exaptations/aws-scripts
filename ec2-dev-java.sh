0f44013b08212940c39c32de77975c450830390e
Mark Ayad <mayad365@gmail.com>
2020-09-12 16:21:27 +0100

    Updated script for ec2_user profile

--- ec2-dev-java.sh
+++ ec2-dev-java.sh
@@ -8,12 +8,12 @@ sudo yum install java-1.8.0-openjdk -y
 sudo amazon-linux-extras install java-openjdk11 -y
 wget https://services.gradle.org/distributions/gradle-6.6.1-bin.zip 
 sudo mkdir /opt/gradle && sudo unzip gradle-6.6.1-bin.zip -d /opt/gradle
-echo 'GRADLE_HOME=/opt/gradle/gradle-6.6.1/bin' >>~/.bash_profile
-echo 'PATH=$GRADLE_HOME:$PATH' >>~/.bash_profile
-source ~/.bash_profile
+echo 'GRADLE_HOME=/opt/gradle/gradle-6.6.1/bin' >>~ec2-user/.bash_profile
+echo 'PATH=$GRADLE_HOME:$PATH' >>~ec2-user/.bash_profile
+# source ~/.bash_profile
 wget https://mirrors.ukfast.co.uk/sites/ftp.apache.org/maven/maven-3/3.6.3/binaries/apache-maven-3.6.3-bin.tar.gz
 sudo mkdir /opt/maven && sudo tar -zxf apache-maven-3.6.3-bin.tar.gz --directory /opt/maven/
-echo 'M2_HOME=/opt/maven/apache-maven-3.6.3' >>~/.bash_profile
-echo 'M2=$M2_HOME/bin' >>~/.bash_profile
-echo 'PATH=$M2:$PATH' >>~/.bash_profile
-source ~/.bash_profile
+echo 'M2_HOME=/opt/maven/apache-maven-3.6.3' >>~ec2-user/.bash_profile
+echo 'M2=$M2_HOME/bin' >>~ec2-user/.bash_profile
+echo 'PATH=$M2:$PATH' >>~ec2-user/.bash_profile
+# source ~/.bash_profile
